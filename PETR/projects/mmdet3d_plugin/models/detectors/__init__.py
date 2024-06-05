044b9f8f442746aece1c015978ce952e8b7df023
Tungyen <cony998820@gmail.com>
2024-06-05 09:41:56 -0700

    a

---
+++ PETR/projects/mmdet3d_plugin/models/detectors/__init__.py
@@ -0,0 +1,11 @@
+# ------------------------------------------------------------------------
+# Copyright (c) 2022 megvii-model. All Rights Reserved.
+# ------------------------------------------------------------------------
+# Modified from DETR3D (https://github.com/WangYueFt/detr3d)
+# Copyright (c) 2021 Wang, Yue
+# ------------------------------------------------------------------------
+# Modified from mmdetection3d (https://github.com/open-mmlab/mmdetection3d)
+# Copyright (c) OpenMMLab. All rights reserved.
+# ------------------------------------------------------------------------
+from .petr3d import Petr3D
+__all__ = ['Petr3D']
